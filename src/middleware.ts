37ba0b7ea8973a3946f61431d62e9fd5832f6382
victorviccente <victorvicente001@hotmail.com>
2025-04-19 23:08:38 -0300

    feat: add new matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,5 +21,10 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
