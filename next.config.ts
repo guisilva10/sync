46bca4579080fe7d68f100affcf06d2421c6f561
guisilva10 <guisilva.070104@gmail.com>
2025-05-01 21:33:46 -0300

    chore: add layout camp in db

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["xzjustnmdbovgqctuhhb.supabase.co"],
+  },
+};
 
 export default nextConfig;
